c57e6110e1686dcc82eb0d92081a51acc007c300
Luka <65416725+Akulias92@users.noreply.github.com>
2021-06-26 10:32:13 +0200

    Update Calculator.java

--- src/JCPTermin3/Calculator.java
+++ src/JCPTermin3/Calculator.java
@@ -9,11 +9,11 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Unesite x: ");
+        System.out.print("Enter x: ");
         int x = sc.nextInt();
-        System.out.print("Unesite y: ");
+        System.out.print("Enter y: ");
         int y = sc.nextInt();
-        System.out.println("Unesite operator +,-,/ ili * ");
+        System.out.println("Enter operator +,-,/ ili * ");
         String operator = sc.next();
         switch(operator){
             case "+":
